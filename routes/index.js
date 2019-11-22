1447c1b71eeefd47b50f0605947045190f78a710
itaske <udochukwupatric@gmail.com>
2019-11-22 22:28:19 +0100

    Added script in packge.json

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/hello', function(req, res, next) {
-  res.send('index');
+router.get('/', function(req, res, next) {
+  res.send('Welcome to Teamwork backend');
 });
 
 module.exports = router;
